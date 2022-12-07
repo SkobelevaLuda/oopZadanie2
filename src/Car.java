00ec9852e6f75056139570cca0e1f8ebf0b10275
Людок <skobeleva.ludmila@yandex.ru>
2022-12-07 14:54:40 +0300

    Задание 3 выполнено

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int year;
+    String country;
+
+    Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+    void cars() {
+        System.out.println(brand + model + engineVolume + color + year + country);
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Car lada = new Car("Lada ", "Granta, ", 1.7, " желтая, ", 2015,
+                " Россия");
+        lada.cars();
+        Car audi = new Car("Audi ", "A8 50 L TDI quattro ", 3.0, " черная, ",
+                2020, " Германия");
+        audi.cars();
+
+        Car bmv = new Car("BMW ", "Z 8, ", 3.0, " черная, ", 2021,
+                " Германия");
+        bmv.cars();
+
+        Car kia = new Car("Kia ", "Sportage 4-го поколения, ", 2.4, " красная, ",
+                2015, " Южная Корея");
+        kia.cars();
+
+        Car hyundai = new Car("Hyundai ", "Avante, ", 1.7, " оранжевая, ",
+                2016, " Южная Корея");
+        hyundai.cars();
+
+    }
 }
