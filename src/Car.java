1debb88bf16b61e6f166def7ee1bfe6d1fe9cf8f
Людок <skobeleva.ludmila@yandex.ru>
2022-12-09 01:33:30 +0300

    Задание 3 выполнено

--- src/Car.java
+++ src/Car.java
@@ -1,41 +1,146 @@
 public class Car {
-    String brand;
-    String model;
-    double engineVolume;
-    String color;
-    int year;
-    String country;
+    public final String brand;
+    private final String model;
+    private double engineVolume;
+    private String color;
+    private final int year;
+    private final String country;
 
-    Car(String brand, String model, double engineVolume, String color, int year, String country) {
+    private String transmission;
+    private final String bodyType;
+    private String registrtionNumber;
+    private final int numberOfSeats;
+    private boolean typeOfRubber;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public String getRegistrtionNumber() {
+        return registrtionNumber;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public boolean getTypeOfRubber() {
+        return typeOfRubber;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+    }
+
+    public void setColor(String color) {
+        if (color == null || color.isBlank()) {
+            this.color = "Белый";
+        } else {
+            this.color = color;
+        }
+    }
+
+    public void setTransmission(String transmission) {
+        if (transmission==null||transmission.isBlank()){
+            this.transmission = "Тип трансмиссии не указан";
+        }else {
+            this.transmission = transmission;
+        }
+    }
+
+    public void setRegistrtionNumber(String registrtionNumber) {
+        if (registrtionNumber==null||registrtionNumber.isBlank()){
+            this.registrtionNumber= " Регистрационный номер не указан";
+        }else {
+            this.registrtionNumber = registrtionNumber;
+        }
+    }
+
+    public void setTypeOfRubber(String typeOfRubber) {
+        this.typeOfRubber = Boolean.parseBoolean(typeOfRubber);
+    }
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country,
+               String transmission, String bodyType, String registrtionNumber, int numberOfSeats, String typeOfRubber) {
         this.brand = brand;
-        if (this.brand==null){
-            this.brand="default";
-        }
         this.model = model;
-        if (this.model==null){
-            this.model="default";
-        }
         this.engineVolume = engineVolume;
-        if (this.engineVolume<=0){
-            this.engineVolume=1.5;
-        }
-        this.color = color;
-        if (this.color==null){
-            this.color="Белый";
-        }
+        setEngineVolume(engineVolume);
+        setColor(color);
         this.year = year;
-        if (this.year<=0){
-            this.year=2000;
-        }
         this.country = country;
-        if (this.country==null){
-            this.country="default";
-        }
+        setTransmission(transmission);
+        this.bodyType = bodyType;
+        setRegistrtionNumber(registrtionNumber);
+        this.numberOfSeats = numberOfSeats;
+        setTypeOfRubber(typeOfRubber);
     }
 
-    void cars() {
-        System.out.println(brand + model + engineVolume + color + year + country);
+    public void changeTyres(){
+        setTypeOfRubber(String.valueOf(!typeOfRubber));
     }
-
-
+    public boolean isValidNumber(){
+        boolean result=true;
+        result=result && registrtionNumber.length()==9;
+        result=result && Character.isLetter(registrtionNumber.charAt(0));
+        result=result && Character.isDigit(registrtionNumber.charAt(1));
+        result=result && Character.isDigit(registrtionNumber.charAt(2));
+        result=result && Character.isDigit(registrtionNumber.charAt(3));
+        result=result && Character.isLetter(registrtionNumber.charAt(4));
+        result=result && Character.isLetter(registrtionNumber.charAt(5));
+        result=result && Character.isDigit(registrtionNumber.charAt(6));
+        result=result && Character.isDigit(registrtionNumber.charAt(7));
+        result=result && Character.isDigit(registrtionNumber.charAt(8));
+        return result;
+    }
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", country='" + country + '\'' +
+                ", transmission='" + transmission + '\'' +
+                ", bodyType='" + bodyType + '\'' +
+                ", registrtionNumber='" + registrtionNumber + '\'' +
+                ", numberOfSeats=" + numberOfSeats +
+                ", typeOfRubber=" + typeOfRubber +
+                '}';
+    }
+    public void print(){
+        System.out.println(this);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,47 @@
+import animals.Cat;
+
 public class Main {
     public static void main(String[] args) {
         Car lada = new Car("Lada ", "Granta, ", 1.7, " желтая, ", 2015,
-                " Россия");
-        lada.cars();
+                " Россия", " автомат ", " седан", А778КК777, 5,
+                "зима");
+
         Car audi = new Car("Audi ", "A8 50 L TDI quattro ", -3.0, " черная, ",
-                2020, " Германия");
-        audi.cars();
+                2020, " Германия", "автомат ", " седан, ", "Е879ВК71",
+                5, "зима");
+
 
         Car bmv = new Car("BMW ", "Z 8, ", 3.0, " черная, ", 2021,
-                " Германия");
-        bmv.cars();
+                " Германия", "автомат", " седан ", " А255КК55 ", 5,
+                " зима");
+
 
         Car kia = new Car("Kia ", "Sportage 4-го поколения, ", 2.4, " красная, ",
-                2015, " Южная Корея");
-        kia.cars();
+                2015, " Южная Корея", "робот", "хечбек", "Т997ТТ99",
+                5, "лето");
 
         Car hyundai = new Car("Hyundai ", "Avante, ", 1.7, " оранжевая, ",
-                -55, " Южная Корея");
-        hyundai.cars();
+                -55, " Южная Корея", "механическая", "седан", "Н556АА17",
+                5, "лето");
+        System.out.println(lada.);
 
+
+
+
+
+
+        Cat myrzik = new Cat("Мурзик");
+        myrzik.getAge();
+        myrzik.meow();
+
+        Cat begemot = new Cat("Бегемот",-5);
+        begemot.meow();
+
+        System.out.println(" У кота "+myrzik.getName()+" "+myrzik.getFriends().length+" друзей");
+        myrzik.addFriend (begemot);
+
+        System.out.println(" У кота "+myrzik.getName()+" "+myrzik.getFriends().length+" друзей");
+        //myrzik.friends[]meow;
     }
+
 }
